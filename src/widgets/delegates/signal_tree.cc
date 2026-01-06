9120b603a6786b18fe79a9df5320fcb8a27b32f2
Dean Lee <deanlee3@gmail.com>
2026-01-06 22:17:26 +0800

    Improve row selection contrast for signal icons (#46)

--- src/widgets/delegates/signal_tree.cc
+++ src/widgets/delegates/signal_tree.cc
@@ -253,7 +253,7 @@ void SignalTreeDelegate::drawButtons(QPainter* p, const QStyleOptionViewItem& op
     QSize logicalSize(BTN_WIDTH - 6, BTN_WIDTH - 6);
     QSize physicalSize = logicalSize * dpr;
 
-    QColor icon_color = active ? opt.palette.color(QPalette::HighlightedText) : opt.palette.color(QPalette::Text);
+    QColor icon_color = (active || (opt.state & QStyle::State_Selected)) ? opt.palette.color(QPalette::HighlightedText) : opt.palette.color(QPalette::Text);
     QPixmap pix = utils::icon(iconName, physicalSize, icon_color);
     pix.setDevicePixelRatio(dpr);
     p->setRenderHint(QPainter::SmoothPixmapTransform);
